2d91e2ecd1316a6177e12b008d6c75452646ac3f
BaekSeongYeon <cln9654@gmail.com>
2022-12-15 16:55:06 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2> Let's develop management system!</h2>
+const customers=[{
+  'id':'1',
+  'image':'https://placeimg.com/64/64/any/1',
+  'name':'홍길동',
+  'birthday':'940720',
+  'gender':'남자',
+  'job':'학생'
+},
+{
+  'id':'2',
+  'image':'https://placeimg.com/64/64/any/2',
+  'name':'백길동',
+  'birthday':'840720',
+  'gender':'남자',
+  'job':'학생'
+},
+{
+  'id':'3',
+  'image':'https://placeimg.com/64/64/any/3',
+  'name':'이길동',
+  'birthday':'740720',
+  'gender':'남자',
+  'job':'학생'
+}
+]
+
+class App extends Component{
+  render() {
+   return (
+      <div>
+      {customers.map(c=>{
+          return(
+            <Customer 
+              key={c.id}
+              id={c.id}
+              imge={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
     </div>
   );
 }
+}
+
 
 export default App;
